7b1e345a195681455e4bd5fb661d7922d26c79b5
Cyrille YAO <cyaoyapi@gmail.com>
2018-08-06 20:17:43 +0200

    Program fetch : version in which we add the prefix 'http://' to the url if is missing

---
+++ gopl.io/chp1/fetch_exer1.8/main.go
@@ -0,0 +1,33 @@
+// This program prints the content found at a URL
+// Add prefix "http://" to the given url if is missing
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error : %v\n", err)
+			os.Exit(1)
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error : %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s\n", body)
+
+	}
+}
