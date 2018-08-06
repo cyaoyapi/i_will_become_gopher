a6ca35a1f20992692cc3be6015525f271200957d
Cyrille YAO <cyaoyapi@gmail.com>
2018-08-06 03:26:38 +0200

    Program dup - Version 3 : Using of the function ReadFile of the package io/ioutil

---
+++ gopl.io/chp1/dup3/main.go
@@ -0,0 +1,34 @@
+// This program prints each line that appears more than
+//once preceded by its count.
+// The lines come from given files in command-line
+// Using of function ReadFile of packahe io/ioutil
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error : %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
