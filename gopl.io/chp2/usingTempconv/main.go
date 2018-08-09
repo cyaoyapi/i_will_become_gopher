f07d0252585e9a38e46e23edb69f5cf3505a1ee6
Cyrille YAO <cyaoyapi@gmail.com>
2018-08-09 15:03:41 +0200

    This program converts its command-line numeric arguments into Fahrenheit and Celsuis

---
+++ gopl.io/chp2/usingTempconv/main.go
@@ -0,0 +1,24 @@
+// This program converts its numeric command-line argument
+// to Celsius and Fahrenheit. It uses the package 'tempconv'
+
+package main
+
+import (
+	"fmt"
+	"i_will_become_gopher/gopl.io/chp2/tempconv"
+	"os"
+	"strconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		argFloat64, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error : %v", err)
+			os.Exit(1)
+		}
+		f := tempconv.Fahrenheit(argFloat64)
+		c := tempconv.Celsuis(argFloat64)
+		fmt.Println(argFloat64, " equal ", tempconv.CToF(c), "and ", tempconv.FToC(f))
+	}
+}
