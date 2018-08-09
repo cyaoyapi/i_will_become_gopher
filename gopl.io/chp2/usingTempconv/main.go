ddfecafd6334afcac345a6b32a52f6091c29e233
Cyrille YAO <cyaoyapi@gmail.com>
2018-08-09 16:21:51 +0200

    Enhancement of the program : the user enters values if there are not command-line arguments

--- gopl.io/chp2/usingTempconv/main.go
+++ gopl.io/chp2/usingTempconv/main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"i_will_become_gopher/gopl.io/chp2/tempconv" // local path
 	"os"
@@ -11,14 +12,25 @@ import (
 )
 
 func main() {
-	for _, arg := range os.Args[1:] {
-		argFloat64, err := strconv.ParseFloat(arg, 64)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error : %v", err)
-			os.Exit(1)
+	if len(os.Args[1:]) == 0 {
+		input := bufio.NewScanner(os.Stdin)
+		for input.Scan() {
+			printErrorOrResult(input.Text())
 		}
-		f := tempconv.Fahrenheit(argFloat64)
-		c := tempconv.Celsuis(argFloat64)
-		fmt.Println(argFloat64, " equal ", tempconv.CToF(c), "and ", tempconv.FToC(f))
+	} else {
+		for _, arg := range os.Args[1:] {
+			printErrorOrResult(arg)
+		}
 	}
+}
+
+func printErrorOrResult(s string) {
+	argFloat64, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error : %v", err)
+		os.Exit(1)
+	}
+	f := tempconv.Fahrenheit(argFloat64)
+	c := tempconv.Celsuis(argFloat64)
+	fmt.Println(argFloat64, " equal ", tempconv.CToF(c), "and ", tempconv.FToC(f))
 }
